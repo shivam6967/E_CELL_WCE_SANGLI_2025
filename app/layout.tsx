d1c7069189bb8433c0fca8e9e743f773f69d4296
shivammathapati <156904099+shivam6967@users.noreply.github.com>
2025-01-30 11:36:55 +0530

    Update layout.tsx
    The Layout has been Upadated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import Next.js script component
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -8,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "IIE CELL WCE SANGLI",
-  description: "Innovation Incubation and Enterpeneurship Cell Wce Sangli",
+  description: "Innovation Incubation and Entrepreneurship Cell WCE Sangli",
 };
 
 export default function RootLayout({
@@ -20,17 +21,27 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/clogo.jpg" sizes="any" />
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XJQLX8R9FG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XJQLX8R9FG');
-</script>
       </head>
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XJQLX8R9FG"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XJQLX8R9FG', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
